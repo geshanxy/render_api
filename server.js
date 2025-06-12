63ba2495da088ef04008f6d610c7fb9f9ae54473
geshanxy <132800727+geshanxy@users.noreply.github.com>
2025-06-13 00:40:08 +0800

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ app.use('/api/config', createProxyMiddleware({
   pathRewrite: { '^/api/config': '/config' }
 }));
 
+app.use('/api', createProxyMiddleware({
+  target: 'http://18.163.40.64:8091',
+  changeOrigin: true,
+  pathRewrite: { '^/api': '' }  // 将开头的 /api 替换为空
+}));
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Proxy running on port ${port}`);
