6dbdfcea82f69377d7e61c925767eec5bface333
geshanxy <132800727+geshanxy@users.noreply.github.com>
2025-06-13 00:46:42 +0800

    Update server.js

--- server.js
+++ server.js
@@ -12,8 +12,19 @@ app.use('/api/config', createProxyMiddleware({
 app.use('/api', createProxyMiddleware({
   target: 'http://18.163.40.64:8091',
   changeOrigin: true,
-  pathRewrite: { '^/api': '' }  // 将开头的 /api 替换为空
+  pathRewrite: {
+    '^/api': ''
+  },
+  onProxyRes(proxyRes, req, res) {
+    const location = proxyRes.headers['location'];
+    if (location) {
+      // 把重定向地址中的 http://18.163.40.64:8091 替换为你的代理地址
+      const newLocation = location.replace('http://18.163.40.64:8091', 'https://你的代理地址/api');
+      proxyRes.headers['location'] = newLocation;
+    }
+  }
 }));
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
